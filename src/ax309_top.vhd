d3ea6cc9bbb01db66e9104d5a2faff4e4107e5df
Iain Waugh <iwaugh@gmail.com>
2018-12-16 22:37:49 +1100

    Added skeleton architecture: Outputs tied low. Tristate IOs broken into 3 signals

--- src/ax309_top.vhd
+++ src/ax309_top.vhd
@@ -11,13 +11,12 @@
 -- Author(s)     : Iain
 -- File Name     : ax309_top.vhd
 --
--- 
+-- Top level file for the AX309 Spartan 6 LX9 evaluation board
 --
 -------------------------------------------------------------------------------
 
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
 
 entity ax309_top is
   port(
@@ -26,13 +25,13 @@ entity ax309_top is
     i_rst_n   : in std_logic;           -- Pushbutton pin - Debounce this
 
     ---------------------------------------------------------------------------
-    -- Miscellaneous pins
+    -- Miscellaneous
     o_led      : out std_logic_vector(3 downto 0);  -- LEDs
     i_key_in   : in  std_logic_vector(3 downto 0);  -- Pushbutton pins
     o_buzz_out : out std_logic;                     -- Loud!
 
     ---------------------------------------------------------------------------
-    -- SDRAM pins
+    -- SDRAM
     o_sdram_clk  : out std_logic;
     o_sdram_cke  : out std_logic;
     o_sdram_ncs  : out std_logic;
@@ -46,7 +45,7 @@ entity ax309_top is
     io_sdram_dqm : inout std_logic_vector(1 downto 0);
 
     ---------------------------------------------------------------------------
-    -- VGA pins
+    -- VGA
     o_vga_red   : out std_logic_vector(4 downto 0);
     o_vga_green : out std_logic_vector(5 downto 0);
     o_vga_blue  : out std_logic_vector(4 downto 0);
@@ -55,42 +54,42 @@ entity ax309_top is
     o_vga_vs : out std_logic;
 
     ---------------------------------------------------------------------------
-    -- SD Card Connector pins
+    -- SD Card Connector
     -- Error on the PCB:
     --   * The FPGA's nCS is routed to DIn
     --   * The FPGA's DIn is routed to GND, so don't drive it high.
     --   * The nCS pin is tied high with no FPGA connection
     -- i.e. The SD card is always enabled and you can't de-select it.
 
-    - Corrected :
+    -- Corrected :
     o_sd_clk     : out std_logic;
     o_sd_gnd     : out std_logic;
     o_sd_datain  : out std_logic;
     i_sd_dataout : in  std_logic;
 
     ---------------------------------------------------------------------------
-    -- USB Serial RS232 pins
+    -- USB Serial RS232
     i_rs232_rx : in  std_logic;
     o_rs232_tx : out std_logic;
 
     ---------------------------------------------------------------------------
-    -- DS1302 Real-Time Clock pins
+    -- DS1302 Real-Time Clock
     o_ds1302_rst  : out   std_logic;
     o_ds1302_sclk : out   std_logic;
     io_ds1302_sio : inout std_logic;
 
     ---------------------------------------------------------------------------
-    -- I2C EEPROM pins
+    -- I2C EEPROM
     io_scl : inout std_logic;
     io_sda : inout std_logic;
 
     ---------------------------------------------------------------------------
-    -- 6x7 Segment Display Interface pins
+    -- 6x7 Segment Display Interface
     o_smg_data : out std_logic_vector(7 downto 0);
     o_scan_sig : out std_logic_vector(5 downto 0);
 
     ---------------------------------------------------------------------------
-    -- OV2640/OV5640/OV7670 Camera pins
+    -- OV2640/OV5640/OV7670 Camera
     -- (optional - could be GPIOs instead)
     o_cam_rst_n : out std_logic;
     o_cam_pwdn  : out std_logic;
@@ -101,6 +100,96 @@ entity ax309_top is
     i_cam_d     : in  std_logic_vector(7 downto 0);
 
     o_cmos_sclk  : out   std_logic;
-    io_cmos_sdat : inout std_logic;
+    io_cmos_sdat : inout std_logic
     );
 end ax309_top;
+
+architecture ax309_top_rtl of ax309_top is
+
+  -- Tristate breakout signals
+  signal i_ds1302_sio   : std_logic;
+  signal o_ds1302_sio   : std_logic;
+  signal ds1302_sio_out : std_logic;
+
+  signal i_scl   : std_logic;
+  signal o_scl   : std_logic;
+  signal scl_out : std_logic;
+
+  signal i_sda   : std_logic;
+  signal o_sda   : std_logic;
+  signal sda_out : std_logic;
+
+  signal i_cmos_sdat   : std_logic;
+  signal o_cmos_sdat   : std_logic;
+  signal cmos_sdat_out : std_logic;
+
+begin  -- ax309_top_rtl
+
+  o_led      <= (others => '0');        -- LEDs
+  o_buzz_out <= '0';                    -- Loud when '1'!
+
+  ---------------------------------------------------------------------------
+  -- SDRAM
+  o_sdram_clk  <= '0';
+  o_sdram_cke  <= '0';
+  o_sdram_ncs  <= '0';
+  o_sdram_nwe  <= '0';
+  o_sdram_ncas <= '0';
+  o_sdram_nras <= '0';
+
+  o_sdram_ba   <= (others => '0');
+  o_sdram_a    <= (others => '0');
+  io_sdram_d   <= (others => '0');
+  io_sdram_dqm <= (others => '0');
+
+  ---------------------------------------------------------------------------
+  -- VGA
+  o_vga_hs <= '0';
+  o_vga_vs <= '0';
+
+  o_vga_red   <= (others => '0');
+  o_vga_green <= (others => '0');
+  o_vga_blue  <= (others => '0');
+
+  ---------------------------------------------------------------------------
+  -- SD Card Connector
+  o_sd_clk    <= '0';
+  o_sd_gnd    <= '0';
+  o_sd_datain <= '0';
+
+  ---------------------------------------------------------------------------
+  -- USB Serial RS232
+  o_rs232_tx <= '0';
+
+  ---------------------------------------------------------------------------
+  -- DS1302 Real-Time Clock
+  o_ds1302_rst  <= '0';
+  o_ds1302_sclk <= '0';
+  io_ds1302_sio <= o_ds1302_sio when ds1302_sio_out = '1' else 'Z';
+  i_ds1302_sio  <= io_ds1302_sio;
+
+  ---------------------------------------------------------------------------
+  -- I2C EEPROM
+  io_scl <= o_scl when scl_out = '1' else 'Z';
+  i_scl  <= io_scl;
+
+  io_sda <= o_sda when sda_out = '1' else 'Z';
+  i_sda  <= io_sda;
+
+  ---------------------------------------------------------------------------
+  -- 6x7 Segment Display Interface
+  o_smg_data <= (others => '0');
+  o_scan_sig <= (others => '0');
+
+  ---------------------------------------------------------------------------
+  -- OV2640/OV5640/OV7670 Camera
+  -- (optional - could be GPIOs instead)
+  o_cam_rst_n <= '0';
+  o_cam_pwdn  <= '0';
+  o_cam_xclk  <= '0';
+
+  o_cmos_sclk  <= '0';
+  io_cmos_sdat <= o_cmos_sdat when cmos_sdat_out = '1' else 'Z';
+  i_cmos_sdat  <= io_cmos_sdat;
+
+end ax309_top_rtl;
