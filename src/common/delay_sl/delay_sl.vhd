a006358862b56fdfd3e5036fb7561c95f21c9bfd
Iain Waugh <iwaugh@gmail.com>
2019-01-27 17:44:54 +1100

    Add some basic utilities

---
+++ src/common/delay_sl/delay_sl.vhd
@@ -0,0 +1,103 @@
+-------------------------------------------------------------------------------
+--
+-- Copyright (c) 2017 CadHut
+-- All rights reserved.
+--
+-- Redistribution and use of this file is permitted.
+--
+-------------------------------------------------------------------------------
+-- Project Name  : CadHut Training: 16 - Inferring Logic
+-- Author(s)     : Iain Waugh
+-- File Name     : delay_sl.vhd
+--
+-- Inferring shift registers, with a register on the final stage.
+-- 
+-- Note: This file uses 'block' keywords which lets us create signals as needed.
+--       If we didn't do this and just declared a general 'sreg' signal at the
+--       top, the we would get synthesis warnings when G_DELAY = 0 or 1 as the
+--       unused bits were dissolved away.
+--
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity delay_sl is
+  generic(
+    G_DELAY : natural := 16
+    );
+  port(
+    -- Clock and Reset signals
+    clk : in std_logic;
+    en  : in std_logic;
+
+    -- Data signals
+    i_data : in  std_logic;
+    o_data : out std_logic
+    );
+end delay_sl;
+
+architecture delay_sl_rtl of delay_sl is
+
+begin
+
+  -----------------------------------------------------------------------------
+  -- No delay: Just connect the input to the output
+  u_no_delay : if G_DELAY = 0 generate
+    o_data <= i_data;
+  end generate u_no_delay;
+
+  -----------------------------------------------------------------------------
+  -- 1 clock delay: Just register it once
+  u_delay_1clk : if G_DELAY = 1 generate
+    delay_block : block is
+      -- Define local signals
+      signal data : std_logic;
+    begin  -- block
+
+      process (clk)
+      begin
+        if (rising_edge(clk)) then
+          if (en = '1') then
+            data <= i_data;
+          end if;
+        end if;
+      end process;
+      o_data <= data;
+
+    end block delay_block;
+  end generate u_delay_1clk;
+
+  -----------------------------------------------------------------------------
+  -- n clock delays: All bits are in a shif-register except for
+  --                 the last bit, which is registered
+  u_delay_n_clks : if G_DELAY > 1 generate
+    delay_block : block is
+      -- Define local signals
+      type t_delay_line is array (1 to G_DELAY - 1) of std_logic;
+      signal sreg : t_delay_line := (others => '0');
+    begin  -- block
+
+      process (clk)
+      begin
+        if (rising_edge(clk)) then
+          if (en = '1') then
+            sreg <= i_data & sreg(1 to sreg'high - 1);
+          end if;
+        end if;
+      end process;
+
+      -- Register the last bit for better fMax
+      process (clk)
+      begin
+        if (rising_edge(clk)) then
+          if (en = '1') then
+            o_data <= sreg(sreg'high);
+          end if;
+        end if;
+      end process;
+
+    end block delay_block;
+  end generate u_delay_n_clks;
+
+end delay_sl_rtl;
---
+++ src/common/pkg/util_pkg.vhd
@@ -0,0 +1,183 @@
+-------------------------------------------------------------------------------
+--
+-- Copyright (c) 2015 CadHut
+-- All rights reserved.
+--
+-- Redistribution and use in source and binary forms (with or without
+-- modification) is not permitted.
+--
+-------------------------------------------------------------------------------
+-- Project Name  : Common Code
+-- Author(s)     : Iain Waugh
+-- File Name     : util_pkg.vhd
+--
+-- Utilities package for common functions
+--
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+package util_pkg is
+
+  -- Maths functions
+  function clog2(x : natural) return natural;
+
+  function maximum(l, r : integer) return integer;
+  function minimum(l, r : integer) return integer;
+
+  -- Bit ordering functions
+  function swap_bit(slv  : std_logic_vector) return std_logic_vector;
+  function swap_byte(slv : std_logic_vector) return std_logic_vector;
+
+  -- Misc functions
+  function ones(x  : natural)  return std_logic_vector;
+  function ones(x  : unsigned) return unsigned;
+  function zeros(x : natural)  return std_logic_vector;
+  function zeros(x : unsigned) return unsigned;
+
+  -- Type conversion functions
+  function to_integer(sl : std_logic) return integer;
+
+  -- Logic functions
+  function and_all(slv  : std_logic_vector) return std_logic;
+  function nand_all(slv : std_logic_vector) return std_logic;
+  function or_all(slv   : std_logic_vector) return std_logic;
+  function nor_all(slv  : std_logic_vector) return std_logic;
+  function xor_all(slv  : std_logic_vector) return std_logic;
+  function xnor_all(slv : std_logic_vector) return std_logic;
+
+end util_pkg;
+
+package body util_pkg is
+
+  -------------------------------------------------------------------------
+  -- Maths functions
+  -------------------------------------------------------------------------
+
+  -- Ceiling LOG2 = number of bits required to represent the natural input
+  function clog2 (x : natural) return natural is
+    variable temp : natural := x;
+    variable n    : natural := 1;
+  begin
+    while temp > 1 loop
+      temp := temp / 2;
+      n    := n+1;
+    end loop;
+    return n;
+  end clog2;
+
+  function maximum(l, r : integer) return integer is
+  begin
+    if l > r then return l; else return r; end if;
+  end maximum;
+
+  function minimum(l, r : integer) return integer is
+  begin
+    if l < r then return l; else return r; end if;
+  end minimum;
+
+  -------------------------------------------------------------------------
+  -- Bit Ordering functions
+  -------------------------------------------------------------------------
+  function swap_bit(slv : std_logic_vector) return std_logic_vector is
+  begin
+    assert (false) report "Function not written yet" severity failure;
+    return slv;
+  end swap_bit;
+
+  function swap_byte(slv : std_logic_vector) return std_logic_vector is
+  begin
+    assert (false) report "Function not written yet" severity failure;
+    return slv;
+  end swap_byte;
+
+  -------------------------------------------------------------------------
+  -- Misc functions
+  -------------------------------------------------------------------------
+  function ones(x : natural) return std_logic_vector is
+    variable slv : std_logic_vector(x - 1 downto 0) := (others => '1');
+  begin
+    return slv;
+  end ones;
+  function ones(x : unsigned) return unsigned is
+    variable u : unsigned(x'range) := (others => '1');
+  begin
+    return u;
+  end ones;
+
+  function zeros(x : natural) return std_logic_vector is
+    variable slv : std_logic_vector(x - 1 downto 0) := (others => '0');
+  begin
+    return slv;
+  end zeros;
+  function zeros(x : unsigned) return unsigned is
+    variable u : unsigned(x'range) := (others => '0');
+  begin
+    return u;
+  end zeros;
+
+  -------------------------------------------------------------------------
+  -- Type Conversion functions
+  -------------------------------------------------------------------------
+  function to_integer(sl : std_logic) return integer is
+  begin
+    if (sl = '0') then
+      return 0;
+    else
+      return 1;
+    end if;
+  end to_integer;
+
+
+  -------------------------------------------------------------------------
+  -- Logic functions
+  -------------------------------------------------------------------------
+
+  function and_all(slv : std_logic_vector) return std_logic is
+    variable r : std_logic;
+  begin
+    r := '1';
+    for i in slv'range loop
+      r := r and slv(i);
+    end loop;
+    return r;
+  end and_all;
+
+  function nand_all(slv : std_logic_vector) return std_logic is
+  begin
+    return not and_all(slv);
+  end nand_all;
+
+  function or_all(slv : std_logic_vector) return std_logic is
+    variable r : std_logic;
+  begin
+    r := '0';
+    for i in slv'range loop
+      r := r or slv(i);
+    end loop;
+    return r;
+  end or_all;
+
+  function nor_all(slv : std_logic_vector) return std_logic is
+  begin
+    return not or_all(slv);
+  end nor_all;
+
+  function xor_all(slv : std_logic_vector) return std_logic is
+    variable r : std_logic;
+  begin
+    r := '0';
+    for i in slv'range loop
+      r := r xor slv(i);
+    end loop;
+    return r;
+  end xor_all;
+
+  function xnor_all(slv : std_logic_vector) return std_logic is
+  begin
+    return not xor_all(slv);
+  end xnor_all;
+
+end util_pkg;
