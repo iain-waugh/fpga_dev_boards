4ed23f916d32336847cd08c21661f286047cb5bd
Iain Waugh <iwaugh@gmail.com>
2019-02-09 12:41:01 +1100

    Fix: It's UG835, not UG834. Added generation of utilisation report.

--- syn/zedboard/zedboard_route.tcl
+++ syn/zedboard/zedboard_route.tcl
@@ -1,6 +1,6 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ## Project TCL command file
-# See UG834 and UG894 for details
+# See UG835 and UG894 for details
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -13,7 +13,7 @@ set PROJECT      [ lindex $argv 3 ]
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Open a design checkpoint in a new project
-#   UG834 v2018.3, page 1038
+#   UG835 v2018.3, page 1038
 #   Options used are:
 #     (none)
 open_checkpoint ${OUT_DIR}/${SYNTH_DCP}.dcp
@@ -22,7 +22,7 @@ open_checkpoint ${OUT_DIR}/${SYNTH_DCP}.dcp
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Optimize the current netlist. This will perform the retarget, propconst,
 # sweep and bram_power_opt optimizations by default.
-#   UG834 v2018.3, page 1068
+#   UG835 v2018.3, page 1068
 #   Options used are:
 #     (none)
 opt_design
@@ -30,7 +30,7 @@ opt_design
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Automatically place ports and leaf-level instances.
-#   UG834 v2018.3, page 1085
+#   UG835 v2018.3, page 1085
 #   Options used are:
 #     (none)
 place_design
@@ -38,7 +38,7 @@ place_design
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Route the current design
-#   UG834 v2018.3, page 1483
+#   UG835 v2018.3, page 1483
 #   Options used are:
 #     (none)
 route_design
@@ -46,15 +46,23 @@ route_design
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Check the design for possible timing problems.
-#   UG834 v2018.3, page 123
+#   UG835 v2018.3, page 123
 #   Options used are:
 #     -file : Filename to output results to.
 check_timing -file ${OUT_DIR}/${ROUTE_DCP}_timing.txt
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Generate the FPGA usage report
+#   UG835 v2018.3, page 1414
+#   Options used are:
+#     -file : Filename to output results to.
+report_utilization -file ${OUT_DIR}/${ROUTE_DCP}_util.txt
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Write a checkpoint of the current design.
-#   UG834 v2018.3, page 1771
+#   UG835 v2018.3, page 1771
 #   Options used are:
 #     -force : overwrite existing
 write_checkpoint ${OUT_DIR}/${ROUTE_DCP}.dcp -force
@@ -62,7 +70,7 @@ write_checkpoint ${OUT_DIR}/${ROUTE_DCP}.dcp -force
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Write a bitstream for the current design.
-#   UG834 v2018.3, page 1758
+#   UG835 v2018.3, page 1758
 #   Options used are:
 #     -force : Overwrite existing file
 write_bitstream ${PROJECT}.bit -force
